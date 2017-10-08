5f681afcdb3162927864dc680bc9d5721f74b0cb
Stephen Tate <austin_tate@baylor.edu>
2017-10-08 15:29:19 -0500

    changed error adding to wrong index

--- src/main/java/petfinder/site/endpoint/OwnerEndpoint.java
+++ src/main/java/petfinder/site/endpoint/OwnerEndpoint.java
@@ -77,7 +77,7 @@ public class OwnerEndpoint {
 			Response response = null;
 			try{
 				response = restClient.performRequest("PUT",
-						"/owners/users/" + owner.getUser().getId().toString(),
+						"/users/owners/" + owner.getUser().getId().toString(),
 						Collections.<String, String>emptyMap(),
 						entity
 				);
