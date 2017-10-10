1cd42a751fd2dd27b7ec9c0cdb68de7de88adad1
ryan atanasoff <ryan_atansaoff@baylor.edu>
2017-10-10 16:58:57 -0500

    add unit test that fails intentionally

--- src/test/java/petfinder/site/test/unit/unitTests.java
+++ src/test/java/petfinder/site/test/unit/unitTests.java
@@ -6,13 +6,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.IOException;
+
+import org.elasticsearch.index.mapper.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import petfinder.site.common.user.UserDto;
 
 
 @RunWith(SpringRunner.class)
@@ -23,7 +31,13 @@ public class unitTests {
 	 private MockMvc mockMvc;
 	 
 	 @Test
-	 public void testAlexaEndPoint() throws Exception {
+	 public void testAlexaEndpoint() throws Exception {
 		 this.mockMvc.perform(get("/api/alexa/getSitters")).andDo(print()).andExpect(status().isOk());
 	  }
+
+	 @Test
+	 public void failTest() {
+		 assertEquals(1,2);
+	 }
+
 }
