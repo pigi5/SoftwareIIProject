fe877fab0500eaa38e7659e8bf0cff43debc01cb
Ford Hash <Ford_Hash@baylor.edu>
2017-10-24 22:57:26 -0500

    fixed joseph's array mapping problem

--- src/main/js/searchresults.js
+++ src/main/js/searchresults.js
@@ -4,29 +4,47 @@ import { GetterButton } from 'js/buttons.js';
 import Navbar from 'js/navbar';
 
 export class SearchResults extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sitters: 
+                [
+                    {
+                        name: 'Susie Sitter',
+                        zip: 76798,
+                        rating: 4.4,
+                        pic: 'http://maxpixel.freegreatpicture.com/static/photo/1x/Dog-Pet-Woman-Doberman-Pinscher-Hobby-Portrait-888400.jpg'
+                    },
+                    {
+                        name: 'Simon Sitter',
+                        zip: 76800,
+                        rating: 4.2,
+                        pic: 'https://c.pxhere.com/photos/77/9b/maine_coon_cat_man_pet_longhair_cat_cool_maine_coon_cat_adidas-845518.jpg!d'
+                    }
+                ]
+        }
+    }
+    
+    createSitterCard(curVal, index, array) {
+        return (
+                <div className="card horizontally-centered" key={index} style={{width: '20rem'}}>
+                    <img className="card-img-top" src={curVal.pic} alt="Card image cap" />
+                    <div className="card-body">
+                        <h4 className="card-title">{curVal.name}</h4>
+                        <p className="card-text">Zip: {curVal.zip}</p>
+                        <p className="card-text">{curVal.rating} stars</p>
+                        <a href="#" className="btn btn-primary" data-toggle="modal" data-target="#sitterRequestedModal">Request</a>
+                    </div>
+                </div>
+        );
+    }
+    
     render() {
         return (
             <div>
 				<Navbar pageName="Search" />
             	<div className="container top-buffer-lg">
-		        	<div className="card horizontally-centered" style={{width: '20rem'}}>
-						<img className="card-img-top" src="http://maxpixel.freegreatpicture.com/static/photo/1x/Dog-Pet-Woman-Doberman-Pinscher-Hobby-Portrait-888400.jpg" alt="Card image cap"/>
-						<div className="card-body">
-							<h4 className="card-title">Susie Sitter</h4>
-							<p className="card-text">Zip: 76798</p>
-							<p className="card-text">4.4 stars</p>
-							<a href="#" className="btn btn-primary" data-toggle="modal" data-target="#sitterRequestedModal">Request</a>
-						</div>
-					</div>
-					<div className="card horizontally-centered" style={{width: '20rem'}}>
-						<img className="card-img-top" src="https://c.pxhere.com/photos/77/9b/maine_coon_cat_man_pet_longhair_cat_cool_maine_coon_cat_adidas-845518.jpg!d" alt="Card image cap"/>
-						<div className="card-body">
-							<h4 className="card-title">Simon Sitter</h4>
-							<p className="card-text">Zip: 76800</p>
-							<p className="card-text">4.2 stars</p>
-							<a href="#" className="btn btn-primary" data-toggle="modal" data-target="#sitterRequestedModal">Request</a>
-						</div>
-					</div>
+		        	{this.state.sitter.map(this.createSitterCard)}
 					
 					<div className="modal fade" id="sitterRequestedModal" tabIndex="-1" role="dialog" aria-labelledby="sitterRequestedModalLabel" aria-hidden="true">
 						<div className="modal-dialog" role="document">
