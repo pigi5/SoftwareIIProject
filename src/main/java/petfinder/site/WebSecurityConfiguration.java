5fa6c86e804e9fdd5740249412b06df279c83ccf
Stephen Tate <austin_tate@baylor.edu>
2017-10-26 17:43:16 -0500

    only main page should be avail without auth

--- src/main/java/petfinder/site/WebSecurityConfiguration.java
+++ src/main/java/petfinder/site/WebSecurityConfiguration.java
@@ -92,8 +92,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 					//why have these specific restrictions when we just have
 					//anyrequest().authenticated() which makes all requests
 					//require authentication
-					.antMatchers("/").permitAll()
-					.antMatchers("/#/").permitAll()
 					.antMatchers("/statics/**").permitAll()
 				.anyRequest().authenticated()
 					.and()
