8b53068a3343b92cabea0ef257b3c48b496e7cd0
Stephen Tate <austin_tate@baylor.edu>
2017-10-26 12:38:50 -0500

    added functionality to login endpoint and changed js file to call login endpoint from login modal

--- src/main/java/petfinder/site/endpoint/LoginEndpoint.java
+++ src/main/java/petfinder/site/endpoint/LoginEndpoint.java
@@ -1,5 +1,8 @@
 package petfinder.site.endpoint;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import petfinder.site.common.user.UserDto;
 
@@ -10,6 +13,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
 
+import javax.xml.ws.Response;
+import java.util.List;
+
 /**
  * Created by jlutteringer on 10/10/17.
  */
@@ -17,33 +23,35 @@ import org.springframework.security.core.context.SecurityContextImpl;
 @RequestMapping(value = "/api/login")
 public class LoginEndpoint {
 
+    ObjectMapper mapper = new ObjectMapper();
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
 
+
     @RequestMapping(method = RequestMethod.POST)
-    public String login(@RequestParam (name = "username") String username, @RequestParam (name = "password")String password) {
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
-        Authentication auth = authenticationManager.authenticate(token);
+    public ResponseEntity<String> login(@RequestParam (name = "username") String username, @RequestParam (name = "password")String password) {
 
-        SecurityContextImpl securityContext = new SecurityContextImpl();
-        securityContext.setAuthentication(auth);
-        SecurityContextHolder.setContext(securityContext);
-        return "Success.";
+        ResponseEntity<String> result = UserEndpoint.getUser(username);
+
+        try {
+
+            UserDto user = mapper.readValue(result.getBody().toString(), UserDto.class);
+
+
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+            Authentication auth = authenticationManager.authenticate(token);
+
+            SecurityContextImpl securityContext = new SecurityContextImpl();
+            securityContext.setAuthentication(auth);
+            SecurityContextHolder.setContext(securityContext);
+
+        }catch(Exception e){
+            e.printStackTrace();
+            //TODO- failed authentication handling
+        }
+
+        return result;
     }
-
-    /*
-    @RequestMapping(method = RequestMethod.POST)
-    public String login(@RequestBody UserDto loginDto) {
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
-        Authentication auth = authenticationManager.authenticate(token);
-
-
-
-        SecurityContextImpl securityContext = new SecurityContextImpl();
-        securityContext.setAuthentication(auth);
-        SecurityContextHolder.setContext(securityContext);
-        return "Success.";
-    }
-    */
 }
