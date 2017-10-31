bf2b9e4aab6461ba13199bfe98bec062d2fa8430
collinsj <joseph2036@gmail.com>
2017-10-30 19:53:22 -0500

    Deleted deprecated file; StartAppt looks and works better

--- src/main/js/startappointment.js
+++ src/main/js/startappointment.js
@@ -1,36 +1,82 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { GetterButton } from 'js/buttons.js';
-import { AddPetModal, PetCheckList } from 'js/addpetmodal.js';
-import { connect } from 'react-redux';
+//import { Link } from 'react-router-dom';
+//import { GetterButton } from 'js/buttons.js';
+//import { AddPetModal, PetCheckList } from 'js/addpetmodal.js';
+//import { connect } from 'react-redux';
 import MyNavbar from 'js/navbar';
 
 export class StartAppointment extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-                        searchUrl: '/#/search?',
-                        startDate: '',
-                        endDate: ''
-                     };
+        	searchUrl: '',
+        	startDate: '',
+        	endDate: '',
+        	pets: [
+            	{
+            		name: 'Fido',
+            		type: 'dog',
+            		checked: true
+            	},
+            	{
+            		name: 'Fluffy',
+            		type: 'cat',
+            		checked: true
+            	}
+            ]
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        //this.addVarToUrl = this.addVarToUrl.bind(this);
     }
     
+    addVarToUrl(param) {
+    	if (this.state.searchUrl != '') {
+    		this.state = {
+    			searchUrl: this.state.searchUrl + '&'
+    		};
+    	}
+    	this.state = {
+    		searchUrl: this.state.searchUrl + param
+    	};
+    	
+    	return this.state.searchUrl;
+    }
+
+    createPetCheckbox(curVal, index, array) {
+    	return (
+			<div className="form-check" key={index}>
+				<input className="form-check-input" type="checkbox" id={curVal.name} defaultChecked/> <label className="form-check-label" htmlFor={curVal.name}> {curVal.name}, {curVal.type}</label>
+			</div>
+    	);
+    }
+
     handleChange(event) {
         this.setState({[event.target.name]: event.target.value});
     }
-
+    
     handleSubmit(event) {
     	// TODO
     	this.state = {
-    			startDate: document.getElementById('inputStartDate').value,
-    			endDate: document.getElementById('inputEndDate').value
+    		startDate: document.getElementById('inputStartDate').value,
+    		endDate: document.getElementById('inputEndDate').value
     	};
     	
-    	if (this.state.searchUrl != '/#/search?') this.state.searchUrl += '&';
-    	this.state.searchUrl += 'date=' + this.state.startDate;
+    	this.addVarToUrl('startDate=' + this.state.startDate);
+    	this.addVarToUrl('endDate=' + this.state.endDate);
+    	
+    	for (var i = 0; i < this.state.pets.size; i++) {
+        	if (this.state.pets[i].checked) {
+	    		this.addVarToUrl('pets=' + this.state.pets[i].name);
+        	}
+        }
+    	
+    	this.state = {
+    		searchUrl: '/#/search?' + encodeURI(this.state.searchUrl)
+    	};
+    	
+    	console.log(this.state.searchUrl);
     	
     	window.location.href = this.state.searchUrl;
         
@@ -48,29 +94,35 @@ export class StartAppointment extends React.Component {
 			    		<fieldset className="top-buffer-lg">
 			    			<div className='row'>
 				    			<legend className="col-form-legend col-sm-2">Pets</legend>
-				    			<PetCheckList parent={this} />
+								<div className="col-sm-10">
+									{this.state.pets.map(this.createPetCheckbox)}
+						    			
+								</div>
 			    			</div>
 		                </fieldset>
 
-			  			<fieldset>
+			  			<fieldset className="top-buffer-lg">
 			  				<LocationRadios />
 			  			</fieldset>
+			  			
 			  			<fieldset className="top-buffer-lg">
 			  				<div className='row'>
 					  			<legend className="col-form-legend col-sm-2">Dates</legend>
-				    			<div className="form-group">
-				  		    		<label htmlFor="inputStartDate">Start date</label>
-				  		    		<input type="date" className="form-control date-box-style" id="inputStartDate" placeholder="mm/dd/yyyy"/>
-				  		    	</div>
-				    			<div className="form-group">
-						    		<label htmlFor="inputEndDate">End date</label>
-						    		<input type="date" className="form-control date-box-style" id="inputEndDate" placeholder="mm/dd/yyyy"/>
+					  			<div className="col-sm-10">
+					    			<div className="form-group">
+					  		    		<label htmlFor="inputStartDate">Start date</label>
+					  		    		<input type="date" className="form-control date-box-style" id="inputStartDate" placeholder="mm/dd/yyyy"/>
+					  		    	</div>
+					    			<div className="form-group">
+							    		<label htmlFor="inputEndDate">End date</label>
+							    		<input type="date" className="form-control date-box-style" id="inputEndDate" placeholder="mm/dd/yyyy"/>
+							    	</div>
 						    	</div>
 					    	</div>
 				    	</fieldset>
 						<div className="form-group row">
 					    	<div className="col-sm-10">
-					    		<button type="submit" className="btn btn-primary">Search</button>
+					    		<button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Search</button>
 					    	</div>
 					    </div>
 
@@ -106,21 +158,85 @@ export class LocationRadios extends React.Component {
 
     render() {
         return (
-        	<form className="top-buffer-lg">
+        	<div className="row">
 				<legend className="col-form-legend col-sm-2">Location</legend>
-	  			<div className="form-check top-buffer-sm">
-					<label className="form-check-label">
-						<input className="form-check-input" type="radio" name="locationRadios" id="locationRadiosMy" value="myHouse" defaultChecked/>
-						My house
-					</label>
+				<div className="col-sm-10">
+		  			<div className="form-check">
+						<input className="form-check-input" type="radio" name="locationRadios" id="locationRadiosMy" value="myHouse" defaultChecked/> <label className="form-check-label" htmlFor="locationRadiosMy">My house</label>
+					</div>
+					<div className="form-check">
+						<input className="form-check-input" type="radio" name="locationRadios" id="locationRadiosSitter" value="sitterHouse"/> <label className="form-check-label" htmlFor="locationRadiosSitter">Sitter&#39;s house</label>
+					</div>
 				</div>
-				<div className="form-check">
-					<label className="form-check-label">
-						<input className="form-check-input" type="radio" name="locationRadios" id="locationRadiosSitter" value="sitterHouse"/>
-						Sitter&#39;s house
-					</label>
-				</div>
-        	</form>
+        	</div>
         );
     }
 }
+
+/*
+class PetCheckList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // TODO: retrieve the user's pets from DB
+			/*
+			/api/users/getPets (?)
+			for each pet, the checkbox should be checked
+            */ /*
+            pets: [
+            	{
+            		name: 'Fido',
+            		type: 'dog',
+            		checked: true
+            	},
+            	{
+            		name: 'Fluffy',
+            		type: 'cat',
+            		checked: true
+            	}
+            ]
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+    	/* TODO: send checked pets into search function in order to restrict
+    	 *       sitters to those that match the pet types
+    	 */ /*
+        for (var i = 0; i < this.state.pets.size; i++) {
+        	if (this.state.parent.state.searchUrl != '/#/search?') this.state.parent.state.searchUrl += '&';
+        	this.state.parent.state.searchUrl += 'pets=' + this.state.pets[i].name; 
+        }
+        event.preventDefault();
+    }
+    
+    createPetCheckbox(curVal, index, array) {
+    	return (
+    			<div className="form-check" key={index}>
+    				<input className="form-check-input" type="checkbox" id={curVal.name} defaultChecked/> <label className="form-check-label" htmlFor={curVal.name}> {curVal.name}, {curVal.type}</label>
+				</div>
+    	);
+    }
+
+    render() {
+        return (
+            <div>
+	    		<fieldset className="form-group top-buffer-sm">
+					<div className="row">
+						<div className="col-sm-10">
+							{this.state.pets.map(this.createPetCheckbox)}
+			    			
+						</div>
+					</div>
+				</fieldset>
+            </div>
+        );
+    }
+}
+*/
